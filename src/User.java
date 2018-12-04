332f2f57596eacf2572e04b09f8c6128ba0c7add
jtkawaski <kawaskij@mail.gvsu.edu>
2018-12-04 02:44:20 -0500

    Basic GUI Function

---
+++ src/User.java
@@ -0,0 +1,104 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.ArrayList;
+
+class User {
+    public String realName;
+    public String userName;
+    public String password;
+
+    DataInputStream dis;
+    DataOutputStream dos;
+
+    public ArrayList<User> friends = new ArrayList<User>();
+
+    // For P2P
+    private int port;
+
+
+    /****
+     *
+     * Holds all the information of the User.
+     *
+     ****/
+    public User(String userName, String password, DataInputStream dis, DataOutputStream dos) {
+        this.userName = userName;
+        this.password = password;
+        this.dis = dis;
+        this.dos = dos;
+    }
+
+    public User(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public void setDis(DataInputStream dis) {
+        this.dis = dis;
+    }
+
+    public void setDos(DataOutputStream dos) {
+        this.dos = dos;
+    }
+
+    public String toString(){
+        return ("Username: " + this.getUserName() +
+                "\tPassword: " + this.getPassword());
+    }
+
+    /**
+     * Sends a post for the Main Feed.
+     *
+     * @param post
+     */
+//    public void updateFeed(String post) {
+//
+//        try {
+//            dos.writeUTF("POST" + post);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    /**
+     * Forwards a message.
+     *
+     * @param msg
+     */
+//    public void sendMsg(String msg) {
+//        try {
+//            dos.writeUTF("SENT" + msg);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    /**
+     * Sends a game invite.
+     *
+     * @param challenger
+     */
+//    public void sendBattleRequest(String challenger) {
+//        try {
+//            dos.writeUTF("BATTLE" + challenger);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+}
+
+
+
